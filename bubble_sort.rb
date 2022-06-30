16fe4c75a2cf868aaddb4bb60a07678142951aa2
Vitor Cardoso <vitorcnf64@gmail.com>
2022-06-29 21:00:26 -0300

    changed the variable switch order, script works now.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,17 @@
 def bubble_sort(arr)
   arr.each_with_index do |element, i|
+    sorted = true
     arr.each_with_index do |item, j|
-      j == arr.length - 1 ? break : false
+      j == arr.length - i - 1 ? break : false # random memory touch prevention
 
       if arr[j] > arr[j + 1]
         tmp = arr[j]
         arr[j] = arr[j + 1]
         arr[j + 1] = tmp
+        sorted = false
       end
     end
+    break if sorted
   end
   arr
 end
@@ -18,7 +21,7 @@ p bubble_sort([4,3,78,2,0,2])
 # pseudo
 # loop n - 1 times
   # for i from 0 to n - 2
-    # if ith > i + 1th, swap them
+    # if ith > ith + 1, swap them
   # end
   # if no swaps were made, break
 # end
